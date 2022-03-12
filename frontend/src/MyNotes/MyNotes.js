784df0b5310d0408f7c906b67a46b040fa2167c9
Jeya Murugan <jeyamuruganvs@gmail.com>
2022-03-12 23:47:00 +0530

    Added my list of notes

--- frontend/src/MyNotes/MyNotes.js
+++ frontend/src/MyNotes/MyNotes.js
@@ -1,8 +1,47 @@
-import React from 'react'
-import MainScreen from '../components/MainScreen'
+import React from 'react';
+import { Badge, Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import MainScreen from '../components/MainScreen';
+
+import notes from '../notes';
 
 const MyNotes = () => {
-  return <MainScreen title="Welcome back Jai">My Notes</MainScreen>
+
+  const deleteHandler = (id) => {
+   if(window.confirm("Are you sure to delete")){
+
+   } 
+  }
+
+  return <MainScreen title="Welcome back Jai">
+    <Link to='createnote'>
+      <Button size='lg'>
+        Create New Note
+      </Button>
+
+      {
+        notes.map(note => (
+          <Card key={note._id} style={{ margin: "10px" }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span style={{ flex: "auto" }}>{note.title}</span>
+              <div>
+                <Button href={`/note/${note._id}`}>Edit</Button>
+                <Button variant='danger' className='mx-2' onClick={()=>deleteHandler(note._id)}>Delete</Button>
+              </div>
+            </Card.Header>
+            <Card.Body>
+              <h4>
+              <Badge variant="success">
+                  Category - {note.category}
+              </Badge>
+              </h4>
+              <p>{note.content}</p>
+            </Card.Body>
+          </Card>
+        ))
+      }
+    </Link>
+  </MainScreen>
 }
 
 export default MyNotes
